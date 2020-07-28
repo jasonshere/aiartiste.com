f0c72899d0c5f5a586268911e38f54bd0a286ef7
jasonshere <mrjasonedu@gmail.com>
2020-07-29 08:53:32 +1000

    add plotly

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,12 +4,10 @@ import Plot from "react-plotly.js";
 const Chart = ({ data, layout }) =>{
 
   return (
-    <div>
-      <Plot
-          data={data}
-          layout={layout}
-        />
-    </div>
+    <Plot
+      data={data}
+      layout={layout}
+    />
   );
 }
 
